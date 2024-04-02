3077ccb0691a5ac58bc726f8f2cd01001f2c2a51
brunoGao <gaojunivas@gmail.com>
2024-04-02 19:06:45 +0800

    small changes... yet to make zhihu and jianshu ready

--- flask_app.py
+++ flask_app.py
@@ -15,13 +15,45 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     keyword = request.form['keyword']
-    blogs = search_juejin(keyword)
-    # 这里可以添加其他网站的搜索结果，并进行去重和排序
-    return render_template('search.html', blogs=blogs)
+    source = request.form.get('source', 'all')  # Default to 'all' if source is not provided
+    print("source: " + source)
 
+    blogs = []
+    if source in ['all', 'juejin']:
+        blogs.extend(search_juejin(keyword))
+        base_url = "https://juejin.cn"
+    if source in ['all', 'csdn']:
+        blogs.extend(search_csdn(keyword))
+        base_url = "https://blog.csdn.net"
+
+    # Optionally, sort or process blogs list as needed
+
+    # Remove duplicates based on title and brief
+    unique_blogs = []
+    seen = set()
+    for blog in blogs:
+        title_brief_tuple = (blog['title'], blog['brief'])
+        if title_brief_tuple not in seen:
+            unique_blogs.append(blog)
+            seen.add(title_brief_tuple)
+    blogs = unique_blogs
+    
+  
+    #blogs.sort(key=custom_sort)
+    blogs.sort(key=lambda blog: blog['last_modify'], reverse=False)
+
+    return render_template('search.html', blogs=blogs, search_query=keyword, source=source, base_url=base_url)
+def custom_sort(blog):
+        return blog['last_modify'] * 0.6 + (-int(blog['digg_count'])) * 0.4
 # 定义过滤器
-def days_since(timestamp):
-    mtime_datetime = datetime.utcfromtimestamp(int(timestamp))
+def days_since(timestamp):  
+    if timestamp.find("-") != -1:
+        print("timestamp:str:: " + str(timestamp))
+        mtime_datetime = datetime.strptime(timestamp, "%Y-%m-%d")
+    else:
+        print("timestamp:int:: " + str(timestamp))
+        mtime_datetime = datetime.utcfromtimestamp(int(timestamp))
+  
     now_datetime = datetime.utcnow()
     difference = now_datetime - mtime_datetime
     return difference.days
@@ -32,12 +64,40 @@ app.jinja_env.filters['days_since'] = days_since
 def changeImageUrl(url):
     uploader = PictureUploader(config_path='uploader_config.ini')
     url = uploader.upload_picture(pic_url=url.split("#")[0]) if (url.startswith("http")) else url
-    
-    url = uploader.upload_picture(url) if url.startswith("http") else url
     return url
 # 注册过滤器
 app.jinja_env.filters['changeImageUrl'] = changeImageUrl
 
+def search_csdn(keyword):
+    base_url = "https://blog.csdn.net"
+    base_name = "CSDN"
+    url = f"https://so.csdn.net/api/v3/search?q={keyword}&t=all&p=1&s=0&tm=0&lv=-1&ft=0&l=&u=&ct=-1&pnt=-1&ry=-1&ss=-1&dct=-1&vco=-1&cc=-1&sc=-1&akt=-1&art=-1&ca=-1&prs=&pre=&ecc=-1&ebc=-1&urw=&ia=1&dId=&cl=-1&scl=-1&tcl=-1&platform=pc&ab_test_code_overlap=&ab_test_random_code="
+    print("url: " + url)
+    headers = {'Content-Type': 'application/json'}
+    response = requests.get(url, headers=headers)
+    blogs = []  # Initialize blogs list
+    if response.status_code == 200:
+        data = response.json()
+        for item in [item for item in data.get('result_vos', []) if item.get("type") == "blog"]:
+            try:
+    
+                blogs.append({
+                    'title': item.get('title'),
+                    'brief':  item.get('description'),
+                    'user_id': base_url + "/" + item.get('author'),
+                    'user_name': item.get('nickname'),
+                    'digg_count': item.get('view_num'),
+                    'url': item.get('url_location'),
+                    'tags': [{'tag_name': tag} for tag in item.get('search_tag')] if item.get('search_tag') else [],
+                    'last_modify': days_since(item.get('create_time_str')),
+                    'base_name': base_name,
+                    'base_url': base_url
+                })
+            except ValueError:  # includes simplejson.decoder.JSONDecodeError
+                print('Decoding JSON has failed')
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+    return blogs
 
 def search_juejin(keyword):
     base_url = "https://juejin.cn"
@@ -51,29 +111,31 @@ def search_juejin(keyword):
         try:
             results = response.json()['data']
             for result in results:
-                # Extracting necessary details from each article
-                article_info = result['result_model']['article_info']
-                author_info = result['result_model']['author_user_info']
-                tags = result['result_model'].get('tags', [])
-                # Preparing tags list
-                article_tags = [{'tag_name': tag['tag_name']} for tag in tags]
-                # Processing cover_image with changeImageUrl
-                cover_image_url = article_info.get('cover_image', '')
-                processed_cover_image_url = changeImageUrl(cover_image_url)
-                # Appending article details to blogs list
-                blogs.append({
-                    'title': article_info['title'],
-                    'url': base_url + "/post/" + article_info['article_id'],
-                    'brief': article_info['brief_content'],
-                    'user_id': author_info['user_id'],
-                    'user_name': base_url + author_info['user_name'],
-                    'last_modify': days_since(article_info['mtime']),
-                    'tags': article_tags,
-                    'digg_count': article_info['digg_count'],
-                    'base_name': base_name,
-                    'base_url': base_url,
-                    'imgUrl': processed_cover_image_url
-                })
+                if result["result_type"] == 2:
+                    # Extracting necessary details from each article
+                    article_info = result['result_model']['article_info']
+                    author_info = result['result_model']['author_user_info']
+                    tags = result['result_model'].get('tags', [])
+                    # Preparing tags list
+                    article_tags = [{'tag_name': tag['tag_name']} for tag in tags]
+                    
+                    # Processing cover_image with changeImageUrl
+                    cover_image_url = article_info.get('cover_image', '')
+                    #processed_cover_image_url = changeImageUrl(cover_image_url)
+                    # Appending article details to blogs list
+                    blogs.append({
+                        'title': article_info['title'],
+                        'url': base_url + "/post/" + article_info['article_id'],
+                        'brief': article_info['brief_content'],
+                        'user_id': base_url + "/user/" +author_info['user_id'],
+                        'user_name':  author_info['user_name'],
+                        'last_modify': days_since(article_info['mtime']),
+                        'tags': article_tags,
+                        'digg_count': article_info['digg_count'],
+                        'base_name': base_name,
+                        'base_url': base_url,
+                        'imgUrl': cover_image_url
+                    })
         except ValueError:  # includes simplejson.decoder.JSONDecodeError
             print('Decoding JSON has failed')
     else:
